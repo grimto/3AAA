6bc2027babe79c61e9795c8e55438fc149b04ea1
grimto <safouene.chahed@medtech.tn>
2019-04-16 15:58:10 +0100

    missing files

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,21 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import { MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA} from '@angular/material';
+
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<DialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) { }
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+  ngOnInit() {
+  }
+
+}
